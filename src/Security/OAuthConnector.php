5ad03926a6401b624dc57541e7b3cb25d4e97a35
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-06-25 13:31:28 +0200

    fix: update authentication login

--- src/Security/OAuthConnector.php
+++ src/Security/OAuthConnector.php
@@ -2,30 +2,30 @@
 
 namespace App\Security;
 
-use HWI\Bundle\OAuthBundle\Connect\AccountConnectorInterface;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthAwareUserProviderInterface;
 
-final class OAuthConnector implements AccountConnectorInterface
+final class OAuthConnector implements OAuthAwareUserProviderInterface
 {
-    public function __construct(
-        private readonly EntityManagerInterface $entityManager,
-        private readonly array $properties
-    ) {
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
     }
 
-    public function connect(UserInterface $user, UserResponseInterface $response)
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response): \Symfony\Component\Security\Core\User\UserInterface
     {
-        if (!isset($this->properties[$response->getResourceOwner()->getName()])) {
-            return;
+        $email = $response->getEmail();
+        $user = $this->userRepository->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            throw new UserNotFoundException(sprintf('No user with email "%s" was found.', $email));
         }
 
-        $property = new PropertyAccessor();
-        $property->setValue($user, $this->properties[$response->getResourceOwner()->getName()], $response->getUserIdentifier());
-
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
+        return $user;
     }
 }
