70aa0068787432a24875028016f183c8569487a3
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-06-26 22:38:26 +0200

    refactor: move all user creation logic in UserRegistrationService file

--- src/Security/OAuthFormHandler.php
+++ src/Security/OAuthFormHandler.php
@@ -3,14 +3,18 @@
 namespace App\Security;
 
 use App\Entity\User;
+use UserRegistrationService;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Form\RegistrationFormHandlerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-final readonly class OAuthFormHandler implements RegistrationFormHandlerInterface
+final class OAuthFormHandler implements RegistrationFormHandlerInterface
 {
+
     public function __construct(
         private UserPasswordHasherInterface $userPasswordHasher
     ) {
@@ -19,37 +23,12 @@ final readonly class OAuthFormHandler implements RegistrationFormHandlerInterfac
     public function process(Request $request, FormInterface $form, UserResponseInterface $userInformation): bool
     {
         $user = new User();
-        $user->setEmail($userInformation->getEmail());
-        $user->setUsername($userInformation->getNickname());
-        $user->setFirstName($userInformation->getFirstName());
-        $user->setLastName($userInformation->getLastName());
-
-        $form->setData($user);
-        $form->handleRequest($request);
+        $userRegistrationService = new UserRegistrationService;
+        $userRegistrationService->createUserFromForm($form, $user, $userInformation, $request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $password_regex = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{12,}$/"; 
-            $plainPassword = $form->get('plainPassword')->getData();
-            if(preg_match($password_regex, $plainPassword)) {
-                $agreeTerms = $form->get('agreeTerms')->getData();
-                if($agreeTerms === true) {
-                    // encode the plain password
-                    $user->setPassword(
-                        $this->userPasswordHasher->hashPassword(
-                            $user,
-                            $plainPassword
-                        )
-                    );
-
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
+            return true;
         }
-
         return false;
     }
 }
