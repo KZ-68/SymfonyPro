4fdfd7540691f0fe55e9c058742c28e80855eccd
KZ-68 <zitnik.kevin@gmail.com>
2025-07-11 16:39:35 +0200

    feat: add docker-user-ban configuration file

--- fail2ban/usr/local/bin/custom-init.sh
+++ fail2ban/usr/local/bin/custom-init.sh
@@ -23,11 +23,30 @@ logpath   = /var/log/frankenphp/access.log
 port      = http,https
 findtime = 10m
 bantime   = 60000000
-banaction = iptables-multiport
+protocol = tcp
+chain = DOCKER-USER
 maxretry  = 3
+action = docker-user-ban[name=frankenphp]
 EOF
 else
 echo "Jail 'frankenphp.local' est déjà présent."
+fi
+
+if [ ! -f /data/action.d/docker-user-ban.conf ]; then
+cat <<EOF > /data/action.d/docker-user-ban.conf
+[Definition]
+actionstart =
+actionstop =
+actioncheck =
+actionban = iptables-legacy -A FORWARD -s <ip> -j DROP
+actionunban = iptables-legacy -A FORWARD -s <ip> -j DROP
+
+[Init]
+iptables = iptables-legacy
+name = docker-user-ban
+EOF
+else
+echo "Config de ban 'docker-user-ban.conf' déjà présent."
 fi
 
 echo "✅ Filtre 'frankenphp' généré."
@@ -36,5 +55,6 @@ mkdir -p /etc/fail2ban/jail.d
 
 cp /data/filter.d/*.conf /etc/fail2ban/filter.d/
 cp /data/jail.d/*.local /etc/fail2ban/jail.d/
+cp /data/action.d/*.conf /etc/fail2ban/action.d
 
 exec fail2ban-server -f
