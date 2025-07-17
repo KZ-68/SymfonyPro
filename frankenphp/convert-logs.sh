daba58ef48b9766745dbad7dea6f76180572b111
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-07-17 23:40:57 +0200

    refactor: remove unused script

--- frankenphp/convert-logs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-touch /var/log/frankenphp/access_text.log
-tail -F /var/log/frankenphp/access.log | \
-jq -r '"\(.request.client_ip) - \(.request.method) \(.request.uri)"' >> /var/log/frankenphp/access_text.log
