dfdbf1f60e23ef3439b44d91cb6e4d0cc04d498b
Kevin ZITNIK <zitnik.kevin@gmail.com>
2025-09-02 23:34:06 +0200

    refactor: move verification email to an listener

--- src/Service/UserRegistrationService.php
+++ src/Service/UserRegistrationService.php
@@ -3,10 +3,7 @@
 namespace App\Service;
 
 use App\Entity\User;
-use App\Security\EmailVerifier;
-use Symfony\Component\Mime\Address;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class UserRegistrationService
@@ -14,7 +11,6 @@ final class UserRegistrationService
     public function __construct(
         private UserPasswordHasherInterface $passwordHasher,
         private EntityManagerInterface $em,
-        private EmailVerifier $emailVerifier,
     ) {}
 
     public function register(User $user, string $plainPassword): void
@@ -23,14 +19,5 @@ final class UserRegistrationService
         $user->setAgreedTerms(true);
         $this->em->persist($user);
         $this->em->flush();
-
-        // generate a signed url and email it to the user
-        $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-            (new TemplatedEmail())
-                ->from(new Address('admin@test.com', 'Admin Mail'))
-                ->to((string) $user->getEmail())
-                ->subject('Please Confirm your Email')
-                ->htmlTemplate('registration/confirmation_email.html.twig')
-        );
     }
 }
