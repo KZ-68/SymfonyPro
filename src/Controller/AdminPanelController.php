522398a33279bcffed922ce39310c07c72fbf338
KZ68 <zitnik.kevin@gmail.com>
2025-09-05 16:09:57 +0200

    refactor: change condition in change role modal method

--- src/Controller/AdminPanelController.php
+++ src/Controller/AdminPanelController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserRoleFormType;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminPanelController extends AbstractController
 {
     #[Route(
-        'admin', 
+        '/admin', 
         name: 'app_admin_index', 
         methods:['GET', 'POST']
     )]
@@ -25,7 +27,7 @@ class AdminPanelController extends AbstractController
     }
 
     #[Route(
-        'admin_users_list', 
+        '/admin/users-list', 
         name: 'app_admin_users_list', 
         methods:['GET']
     )]
@@ -37,4 +39,49 @@ class AdminPanelController extends AbstractController
             'users' => $users,
         ]);
     }
+
+    #[Route(
+        '/admin/users-list/popup-role', 
+        name: 'admin_role_popup',
+        methods:['GET', 'POST']
+    )]
+    #[IsGranted('ROLE_ADMIN')]
+    public function popupChangeRole(Request $request, UserRepository $userRepository): Response {
+
+        $selectedUser = $request->get("user");
+
+        if(!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $form = $this->createForm(UserRoleFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $selectedRole =  $form->get('roles')->getData();
+
+            // On recherche le bon utilisateur dans la couche modèle
+            $editUser = $userRepository->find($selectedUser);
+
+            if ($this->getUser() !== $selectedUser) {
+                // On met à jour le rôle par une requête préparée. 
+                $userRepository->updateRoleRequest($editUser->getId(), $selectedRole);
+
+                return $this->redirectToRoute('app_admin_users_list');
+            } else {
+                $this->addFlash('error', 'You can\'t change your own role');
+
+                return $this->redirectToRoute('app_admin_users_list');
+            }
+            
+        }
+
+        return $this->render('admin/_partials/change-role-modal.html.twig', [
+                'editUserForm' => $form
+            ]
+        );
+    }
+
 }
