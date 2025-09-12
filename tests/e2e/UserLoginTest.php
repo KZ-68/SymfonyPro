c562fd82a740fbd064fa95e798f38df0be9ea008
KZ68 <zitnik.kevin@gmail.com>
2025-09-12 14:05:09 +0200

    update: modify panther client configuration for docker environnment

--- tests/e2e/UserLoginTest.php
+++ tests/e2e/UserLoginTest.php
@@ -6,38 +6,34 @@ use Symfony\Component\Panther\Client;
 use Symfony\Component\Panther\PantherTestCase;
 
 class UserLoginTest extends PantherTestCase
+
 {
+
     public function test(): void
     {
         // $client = Client::createChromeClient(__DIR__.'/../../drivers/chromedriver.exe', null, [], 'http://127.0.0.1:8000');
         $client = static::createPantherClient(
-        [
+        options: [
             'browser' => PantherTestCase::CHROME,
-            'hostname' => 'http://127.0.0.1',
-            'port' => '8000',
             'enable_output' => true
         ],
-        [
-            'external_base_uri' => 'http://localhost',
+        kernelOptions: [
+            
         ],
-        [
-            '--headless',
-            '--disable-gpu',
-            '--no-sandbox',
-            '--remote-allow-origins=*'
+        managerOptions: [
         ]
         );
         $client->request('GET', '/home');
         
-        $this->assertPageTitleContains('Symfony Professional Panel');
+        $this->assertPageTitleContains('Home');
 
         $this->assertSelectorIsEnabled('.login-link');
-        $this->assertSelectorIsEnabled('.login-register');
+        $this->assertSelectorIsEnabled('.register-link');
 
         $client->request('GET', '/login');
 
         $this->assertSelectorExists('[type="submit"]');
 
-        $client->submitForm('Sign in', ['email' => 'test@exemple.com', 'password' => '{OO@a2b6}!#_?']);
+        $client->submitForm('Sign in', ['_username' => 'test@exemple.com', '_password' => '{OO@a2b6}!#_?']);
     }
 }
